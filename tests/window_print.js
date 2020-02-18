62af05207fbe8e304ec0449779e98ef5ca62558b
Rob Cooper <rcooper@change.org>
2020-02-18 11:37:24 -0800

    Initial Commit - add html page and tests

---
+++ tests/window_print.js
@@ -0,0 +1,18 @@
+import { t, Selector } from 'testcafe';
+
+fixture("Some test").page("localhost:3000")
+
+test("Click button", async () => {
+    await t.click(Selector("#click_me")).expect(Selector("#heading").innerText).eql("This is a Heading");
+
+})
+
+test("Click button 2", async () => {
+    await t.click(Selector("#click_me")).expect(Selector("#heading").innerText).eql("This is a Heading");
+
+})
+
+test("Click button 3", async () => {
+    await t.click(Selector("#click_me")).expect(Selector("#heading").innerText).eql("This is a Heading");
+
+})
